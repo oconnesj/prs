1a84207c2d902f9d9a1ba963de956d88f85eea3f
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-03-20 15:26:31 -0400

    Update prs_db_create.sql

--- prs_db_create.sql
+++ prs_db_create.sql
@@ -30,14 +30,21 @@ create table User (
 	Status					varchar(20)			not null,
     Total					decimal(10,2)		not null,
 	SubmittedDate			datetime			not null,
-	ReasonForRejection		varchar(100)	   	null
+	ReasonForRejection		varchar(100)	   	null,
+    foreign key (UserID) references User(ID)
     );
+    
+    /*	foreign key (actorID) references actor(ID),
+	foreign key (movieID) references movie(ID),
+    constraint act_mov unique (actorID, movieID)*/
     
     create table LineItem (
     ID						int					primary key auto_increment,
 	RequestID				int					not null,
     ProductID				int					not null,
-	Quanity					int					not null
+	Quanity					int					not null,
+    foreign key (ProductID) references Product(ID),
+    foreign key (RequestID) references Request(ID)
     );
     
     create table Product (
@@ -47,10 +54,24 @@ create table User (
     Name					varchar(150)		not null,
     Price					decimal(10,2)		not null,
     Unit					varchar(255)		null,
-    PhotoPath				varchar(255)		null
+    PhotoPath				varchar(255)		null,
+    foreign key (VendorID) references Vendor(ID)
     );
     
     
     create table Vendor (
+    ID						int					primary key auto_increment,
+    Code					varchar(10)			not null,
+    Name					varchar(255)		not null,
+    Address					varchar(255)		not null,
+    City					varchar(255)		not null,
+    State					varchar(2)			not null,
+    Zip						varchar(5)			not null,
+    PhoneNumber				varchar(12)			not null,
+    Email					varchar(100)		not null
+    );
+    
+    
+    
 	
 	
