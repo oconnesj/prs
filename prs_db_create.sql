cd4569d5c8052f7bb87a43689cec525e37aea9f4
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-03-20 14:55:55 -0400

    Update prs_db_create.sql

--- prs_db_create.sql
+++ prs_db_create.sql
@@ -1,1 +1,19 @@
--- prs -- 
+-- Drop and Create the Database, DDL
+drop database if exists prs_db;
+create database prs_db;
+use prs_db;
+
+
+-- DDL - Create Tables
+
+create table User (
+	ID 				int 			primary key auto_increment,
+	UserName		varchar(20) 	not null,
+	Password		varchar(10)		not null,
+	FirstName		varchar(20) 	not null,		
+	LastName		varchar(20) 	not null,
+    PhoneNumber		varchar(12)     not null,
+    Email			varchar(75)		not null,
+    isReviewer		tinyint			not null,
+    isAdmin			tinyint			not null
+	);
