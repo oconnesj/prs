502ee31afd66c425fa50a320023a692633d28067
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-03-20 15:05:49 -0400

    Update prs_db_create.sql

--- prs_db_create.sql
+++ prs_db_create.sql
@@ -17,3 +17,18 @@ create table User (
     isReviewer		tinyint			not null,
     isAdmin			tinyint			not null
 	);
+    
+    
+ create table Request (
+ 
+	ID						int					primary key auto_increment,
+	UserID					int					not null,
+	Description				varchar(100)		not null,
+	Justification			varchar(255)		not null,
+	DateNeeded				date				not null,
+	DeliveryMode			varchar(25)			not null,
+	Status					varchar(20)			not null,
+    Total					decimal(10,2)		not null,
+	SubmittedDate			datetime			not null,
+	ReasonForRejection		varchar(100)	   	null
+    );
